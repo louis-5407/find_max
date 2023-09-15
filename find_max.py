57189ac0d6ec1bcf955c628de68b84f4386ebe49
louis liao <louis.5407@gmail.com>
2023-09-16 00:49:53 +0800

    直接import套件

--- find_max.py
+++ find_max.py
@@ -1,8 +1,8 @@
-from Math_class import Math	
+import Math_class  
 
 def main():
 	b_list = [-1, 1]
-	math = Math(b_list)
+	math = Math_class.Math(b_list)
 	print("在陣列中", math.list, "哪個最大？")
 
 	math.find_max(b_list)
