eea07af4557ad28ac21c4caf827336eb8c7d3bdc
louis liao <louis.5407@gmail.com>
2023-09-16 00:29:05 +0800

    add_ class

--- find_max.py
+++ find_max.py
@@ -1,18 +1,32 @@
-def find_max(a_list):
-	if not a_list:
-		max_ = 0
-	else:
-		max_ = a_list[0]
+	
+class Math():
+	def __init__(self):
+		pass
 
-	for n in a_list:
-		if n > max_:
-			max_ = n
+	def is_empty_array(self, arr):
+		if not arr:
+			return True
+		else:
+			return False
 
-	print(max_)
+	def find_max(self, a_list):
+		result = self.is_empty_array(a_list)
+
+		if result:
+			max_ = 0
+		else:
+			max_ = a_list[0]
+
+		for n in a_list:
+			if n > max_:
+				max_ = n
+
+		print(max_)
 
 def main():
-	b_list = [-1]
-	find_max(b_list)
+	b_list = [-1, 1]
+	math = Math()
+	math.find_max(b_list)
 
 main()
 
