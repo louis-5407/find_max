206530ce3ef5b5797c7146fe3a32a8e614f87109
louis liao <louis.5407@gmail.com>
2023-09-16 00:11:34 +0800

    true_answer

--- find_max.py
+++ find_max.py
@@ -1,5 +1,8 @@
 def find_max(a_list):
-	max_ = a_list[0]
+	if not a_list:
+		max_ = 0
+	else:
+		max_ = a_list[0]
 
 	for n in a_list:
 		if n > max_:
@@ -8,7 +11,7 @@ def find_max(a_list):
 	print(max_)
 
 def main():
-	b_list = []
+	b_list = [-1]
 	find_max(b_list)
 
 main()
