fac1463131c3a54cc382e8ccc94eaf7ee6b2b3b7
louis liao <louis.5407@gmail.com>
2023-09-16 00:46:16 +0800

    把class分到其他套件

--- find_max.py
+++ find_max.py
@@ -1,27 +1,4 @@
-	
-class Math():
-	def __init__(self, list):
-		self.list = list
-
-	def is_empty_array(self, arr):
-		if not arr:
-			return True
-		else:
-			return False
-
-	def find_max(self, a_list):
-		result = self.is_empty_array(a_list)
-
-		if result:
-			max_ = 0
-		else:
-			max_ = a_list[0]
-
-		for n in a_list:
-			if n > max_:
-				max_ = n
-
-		print(max_)
+from Math_class import Math	
 
 def main():
 	b_list = [-1, 1]
